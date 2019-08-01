624450b1456607ad813aff9719006a02af0a401b
vyshnavrajs2019 <svyshnavraj@gmail.com>
2019-08-01 22:54:00 +0530

    added login functionality

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_view
 from django.urls import path
 from .views import home, register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name = 'home'),
-    path('register/', register, name = 'register')
+    path('register/', register, name = 'register'),
+    path('login/', auth_view.LoginView.as_view(template_name = 'auth/login.html'), name = 'login')
 ]
