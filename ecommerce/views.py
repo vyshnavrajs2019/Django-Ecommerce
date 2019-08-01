83cb8ceb254dc1391f64fa07c67ca968e070210b
vyshnavrajs2019 <svyshnavraj@gmail.com>
2019-08-01 22:42:27 +0530

    register route has been added

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserRegistrationForm
+from django.contrib import messages
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}')
+            return redirect('home')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'auth/register.html',{
+        'form': form
+    })
 
 def home(request):
     return render(request, 'search.html')
