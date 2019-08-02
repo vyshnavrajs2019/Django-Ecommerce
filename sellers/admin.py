124858bb33292722c0bdd1bdf8124e54d8a1360c
vyshnavrajs2019 <svyshnavraj@gmail.com>
2019-08-02 18:23:47 +0530

    created and registered models CheckSeller and Seller

--- sellers/admin.py
+++ sellers/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Seller, CheckSeller
 # Register your models here.
+
+admin.site.register(Seller)
+admin.site.register(CheckSeller)
--- sellers/models.py
+++ sellers/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class CheckSeller(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    is_seller = models.BooleanField(verbose_name = 'seller', default = False)
+
+
+class Seller(models.Model):
+    owner = models.ForeignKey(User, on_delete = models.CASCADE)
+    company = models.CharField(max_length = 200, blank = False, null = False)
+    location = models.CharField(max_length = 350, blank = False, null = False)
+    zipcode = models.CharField(max_length = 6, blank = False, null = False)
+    mobile = models.CharField(max_length = 10, blank = False, null = False)
